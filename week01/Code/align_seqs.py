88d8a664aba5ac3a02ee5aba2721e4f3541c795c
Freda Xiaoyun Yu <shireenyu@163.com>
2023-10-16 12:11:16 +0100

    added_comments_interpretaion

--- week01/Code/align_seqs.py
+++ week01/Code/align_seqs.py
@@ -1,4 +1,4 @@
-# Two example sequences to match
+.# Two example sequences to match
 seq2 = "ATCGCCGGATTACGGG"
 seq1 = "CAATTCGGAT"
 
@@ -8,31 +8,44 @@ seq1 = "CAATTCGGAT"
 l1 = len(seq1)
 l2 = len(seq2)
 if l1 >= l2:
-    s1 = seq1
-    s2 = seq2
-else:
-    s1 = seq2
-    s2 = seq1
+    s1 = seq1  # me: s1 is the longer one
+    s2 = seq2  # me: s2 is the shorter one
+else: 
+    s1 = seq2  # me: s1 is the longer one
+    s2 = seq1  # me: s2 is the shorter one
     l1, l2 = l2, l1 # swap the two lengths
+    
+# me: Here, s1 is the longer one. l1 is the length of s1; l2 is the length of s2. 
+########################################################
 
 # A function that computes a score by returning the number of matches starting
 # from arbitrary startpoint (chosen by user)
+
+
 def calculate_score(s1, s2, l1, l2, startpoint):
+
+    # initialisation: 
     matched = "" # to hold string displaying alignements
     score = 0
-    for i in range(l2):
-        if (i + startpoint) < l1:
+    
+    for i in range(l2):   # me: iterate throught the shorter 
+        if (i + startpoint) < l1:  # me: if the startpoint is within l1
             if s1[i + startpoint] == s2[i]: # if the bases match
-                matched = matched + "*"
-                score = score + 1
+            	# me: compare one by one
+                matched = matched + "*"     # me: you are counting the matched, marking
+                score = score + 1    # me: you are counting
             else:
-                matched = matched + "-"
+                matched = matched + "-"    # me: you are counting the unmatched, marking
 
     # some formatted output
-    print("." * startpoint + matched)           
+    print("." * startpoint + matched)   
+            
     print("." * startpoint + s2)
+    
     print(s1)
+    
     print(score) 
+    
     print(" ")
 
     return score
@@ -54,3 +67,8 @@ for i in range(l1): # Note that you just take the last alignment with the highes
 print(my_best_align)
 print(s1)
 print("Best score:", my_best_score)
+
+
+
+
+
