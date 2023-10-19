5f116defea39e79c2fa06eea5badcb0c81736662
Freda Xiaoyun Yu <shireenyu@163.com>
2023-10-19 16:56:09 +0100

    19Oct_Align_DNA_concept
    Asked Ben

--- week02/Code/align_seqs.py
+++ week02/Code/align_seqs.py
@@ -1,12 +1,18 @@
 
 ###############
+# TASK: insert breakpoint 
+
 # My Interpretation of Align DNA: Align DNA is 拿一个大的作为参照物，拿个小的在它上面，从第一个开始，以大的那个挨个为 start point，挪到最后
 # 给每个比对一个分，要最大的那个分（可能有平局），（我觉得我能匹配DNA片段？）
 # 
 # The translation tells me that 比对两个DNA，让它们尽可能相似
 # 
 # My idea: 最优，是距离。尽可能相似，是距离。 distance, to minimise the distance
+#
+# My tutor Ben tells me that, (1) concatenate, FALSE; (2) match, is like, CORRECT. 
+# My question is, does Align DNA contain the single ATGC fragments?????????????????? 
 
+import ipdb
 
 
 # Two example sequences to match
@@ -36,13 +42,14 @@ else:
 
 
 def calculate_score(s1, s2, l1, l2, startpoint):
+
     '''  me
     Parameters: 
         s1:
         s2:
         l1: 
         l2:
-        startpoint: 
+        startpoint: You choose a startpoint I guess is on the longer DNA
     Output: 
         score: 
 
@@ -52,7 +59,7 @@ def calculate_score(s1, s2, l1, l2, startpoint):
     matched = "" # to hold string displaying alignements
     score = 0
     
-    for i in range(l2):   # me: for each character in the shorter 
+    for i in range(l2):   # me: for each character in the shorter          i?????????????
         if (i + startpoint) < l1:  # me: if the startpoint chosen by user is within l1
             if s1[i + startpoint] == s2[i]: # if the bases match
             	# me: compare one by one
@@ -62,15 +69,17 @@ def calculate_score(s1, s2, l1, l2, startpoint):
                 matched = matched + "-"    # me: you are counting the unmatched, marking
 
     # some formatted output
-    print("." * startpoint + matched)   # me: print out the marked ****---**---**
+    print("Firstly here: ", "." * startpoint + matched)   # me: print out the marked ****---**---**
             
-    print("." * startpoint + s2)   # 
+    print("Look here: ", "." * startpoint + s2)   # CAATTCGGAT
     
-    print(s1)
+    print("s1: : ",s1)
     
-    print(score) 
+    print("score: ", score) 
     
     print(" ")
+
+    print("startpoint: ", startpoint)
 
     return score
 
@@ -84,15 +93,17 @@ my_best_align = None
 my_best_score = -1
 
 for i in range(l1): # Note that you just take the last alignment with the highest score
-    z = calculate_score(s1, s2, l1, l2, i)
+    z = calculate_score(s1, s2, l1, l2, i)     # WE ARE CALLING the calculate_score()!!!!!!!!!!!!!!!!!!!!!!!!!
     if z > my_best_score:
         my_best_align = "." * i + s2 # think about what this is doing!
         my_best_score = z 
+
+
 print(my_best_align)
 print(s1)
 print("Best score:", my_best_score)
 
 
-
+ipdb.set_trace()
 
 
