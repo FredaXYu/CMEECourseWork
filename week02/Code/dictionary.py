a48cebac79fec103ec216be10f37c705d1430432
Freda Xiaoyun Yu <shireenyu@163.com>
2023-10-20 15:30:50 +0100

    Update dictionary.py

--- week02/Code/dictionary.py
+++ week02/Code/dictionary.py
@@ -51,9 +51,27 @@ taxa_dic = {'Myotis lucifugus': 'Chiroptera',
 #          'Canis lupus': 'Carnivora',}"    # now this is the input data
 
 
+def key_value_sepa_to_two(list_key, list_value): 
+        returned_list = []
+        if len(list_key == list_value): 
+                each_pair = []
+                for i in range(len(list_key)): 
+                        each_pair.append(list_key[i], list_value[i])
+                print("each_pair", each_pair)        
+
+                # try
+                returned_list.append(each_pair)
+
+                return returned_list
+        else: 
+                return False
+                
+
+
 # map to not a set, but a list
-def map_order_to_list(dic): 
+def map_order_to_list(dic):    # ok
         thing_return = []
+        dic_keys = []
         output_list_key = []
         output_list_value = []
         # for each in taxa_dic: 
@@ -62,15 +80,46 @@ def map_order_to_list(dic):
         #         output_list_key.append(each.key)
         #         output_list_value.append(each.value)
 
-        for i in range(len(taxa_dic)): 
-                print("type(taxa_dic[i])", type(taxa_dic[i]))    # <class 'str'>
-                print("taxa_dic[i]", taxa_dic[i])         # Myotis lucifugus
-                output_list_key.append(taxa_dic[i].key)
-                output_list_value.append(taxa_dic[i].value)
+        # for i in range(len(taxa_dic)): 
+        #         print("bbbbbb")
+        #         print("The dict name: ", taxa_dic[i])
+        #         print("taxa_dic[i]", taxa_dic[])
+        #         print("type(taxa_dic[i])", type(taxa_dic[i]))    # <class 'str'>
+        #         print("taxa_dic[i]", taxa_dic[i])         # Myotis lucifugus
+        #         output_list_key.append(taxa_dic[i].key)
+        #         output_list_value.append(taxa_dic[i].value)
 
-        print("output_list_key", output_list_key)
-        print("output_list_value", output_list_value)
-        thing_return.append(output_list_key, output_list_value)
+        taxa_in_list = list(taxa_dic.values())
+        output_list_value = taxa_in_list
+        two_key_value = []   # initialisation
+
+        print(taxa_dic.values())
+        print("taxa_in_list", taxa_in_list)
+
+        for each in taxa_dic: 
+                print("bbbbbb")
+                print("taxa_dic[each]", taxa_dic[each])     # have this 
+                print("type(taxa_dic[each])", type(taxa_dic[each]))    # <class 'str'>
+                # print("taxa_dic[each]", taxa_dic[each].key())              
+
+                # output_list_key.append(taxa_dic[each].key)    # don't have this kind of syntax
+                output_list_value.append(taxa_dic[each])  
+
+                two_key_value.append(each)
+
+        dic_keys = dic.keys()
+        print("dic_keys: ", dic_keys)   # can pass 20 Oct 14:59
+        print("output_list_key", output_list_key)    # can pass 20 Oct 14:59
+        print("output_list_value", output_list_value)     # can pass 20 Oct 14:59
+        thing_return.append(output_list_key)
+        thing_return.append(output_list_value)
+
+        print("here")
+
+
+        key_value_sepa_to_two
+
+        
 
         return thing_return
 
@@ -100,4 +149,7 @@ map_order_to_list(taxa_dic)
 # Notes: 
 # 字典的每一个元素是str
 
+##marks = {'Physics':67, 'Maths':87}
 
+# print(marks.values())
+# Output: dict_values([67, 87])
