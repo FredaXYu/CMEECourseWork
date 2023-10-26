24d3db7e0d829a104b1e9ff8a88772f3b66ca2da
Freda Xiaoyun Yu <shireenyu@163.com>
2023-10-26 19:47:02 +0100

    Create 26Oct_as_matrix.r

---
+++ week04/Mycodes/26Oct_as_matrix.r
@@ -0,0 +1,54 @@
+
+#############################
+
+# what data types can as.matrix() accept and ouput? 
+
+# father yky's method: 
+
+x <- c(1, 2, 3, 4, 5)
+y <- c(4, 3, 6, 8, 7)
+
+new <- data.frame(col1 = x, col2 = y)
+
+is.data.frame(new)    # TRUE
+
+mat <- as.matrix(new)   # If we use as.matrix(dataframe), then the output matrix's elements are numeric. 
+
+is.matrix(mat)
+
+mat
+
+# C:\Users\yxy\Github_desktop\CMEECourseWork\week04\Mycodes>Rscript 26Oct_as_matrix.r
+# [1] TRUE
+# [1] TRUE
+# col1 col2
+# [1,]    1    4
+# [2,]    2    3
+# [3,]    3    6
+# [4,]    4    8
+# [5,]    5    7
+
+
+
+#################################################
+
+# My discovers: 
+
+# first, read the txt file, then it's a list
+yuxiao <- read.table("../Data/SparrowSize.txt", header=TRUE)
+# print(yuxiao$Bill)
+# the as.matrix prints out character elements ie. strings
+
+
+#################################################
+# CSDN: 
+
+mat_new <- apply(yuxiao$Bill, 2, as.numeric)
+mat_new2 <- as.matrix(mat_new)
+print("Using apply(): ")
+mat_new2
+
+
+
+
+
