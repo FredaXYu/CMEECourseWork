b8dd44b9ff573713d39f73b611d03a6d3156f936
Freda Xiaoyun Yu <shireenyu@163.com>
2023-11-03 10:42:35 +0000

    .

---
+++ week03/Code/data_mag_vis_plots_teachers.R
@@ -0,0 +1,90 @@
+
+# https://mhasoba.github.io/TheMulQuaBio/notebooks/08-Data_R.html#handling-big-data-in-r
+
+# this file can run and show proper outputs in terminal. 
+
+MyDF <- read.csv("../Data/EcolArchives-E089-51-D1.csv")
+dim(MyDF) #check the size of the data frame you loaded
+
+head(MyDF)
+
+str(MyDF)
+
+require(tidyverse)
+glimpse(MyDF)
+
+MyDF$Type.of.feeding.interaction <- as.factor(MyDF$Type.of.feeding.interaction)
+MyDF$Location <- as.factor(MyDF$Location)
+str(MyDF)
+
+plot(MyDF$Predator.mass,MyDF$Prey.mass)
+
+## use log: 
+plot(log(MyDF$Predator.mass),log(MyDF$Prey.mass))
+# base 10: 
+plot(log10(MyDF$Predator.mass),log10(MyDF$Prey.mass))
+
+plot(log10(MyDF$Predator.mass),log10(MyDF$Prey.mass),pch=20) # Change marker
+
+plot(log10(MyDF$Predator.mass),log10(MyDF$Prey.mass),pch=20, xlab = "Predator Mass (g)", ylab = "Prey Mass (g)") # Add labels
+
+## histogram: 
+hist(MyDF$Predator.mass)
+# log: 
+hist(log10(MyDF$Predator.mass), xlab = "log10(Predator Mass (g))", ylab = "Count") # include labels
+hist(log10(MyDF$Predator.mass),xlab="log10(Predator Mass (g))",ylab="Count", 
+     col = "lightblue", border = "pink") # Change bar and borders colors 
+
+
+par(mfcol=c(2,1)) #initialize multi-paneled plot
+par(mfg = c(1,1)) # specify which sub-plot to use first 
+hist(log10(MyDF$Predator.mass),
+     xlab = "log10(Predator Mass (g))", ylab = "Count", col = "lightblue", border = "pink", 
+     main = 'Predator') # Add title
+par(mfg = c(2,1)) # Second sub-plot
+hist(log10(MyDF$Prey.mass), xlab="log10(Prey Mass (g))",ylab="Count", col = "lightgreen", border = "pink", main = 'prey')
+
+hist(log10(MyDF$Predator.mass), # Predator histogram
+     xlab="log10(Body Mass (g))", ylab="Count", 
+     col = rgb(1, 0, 0, 0.5), # Note 'rgb', fourth value is transparency
+     main = "Predator-prey size Overlap") 
+hist(log10(MyDF$Prey.mass), col = rgb(0, 0, 1, 0.5), add = T) # Plot prey
+legend('topleft',c('Predators','Prey'),   # Add legend
+       fill=c(rgb(1, 0, 0, 0.5), rgb(0, 0, 1, 0.5))) # Define legend colors
+
+
+hist(log10(MyDF$Predator.mass), # Predator histogram
+     xlab="log10(Body Mass (g))", ylab="Count", 
+     col = rgb(1, 0, 0, 0.5), # Note 'rgb', fourth value is transparency
+     main = "Predator-prey size Overlap") 
+hist(log10(MyDF$Prey.mass), col = rgb(0, 0, 1, 0.5), add = T) # Plot prey
+legend('topleft',c('Predators','Prey'),   # Add legend
+       fill=c(rgb(1, 0, 0, 0.5), rgb(0, 0, 1, 0.5))) # Define legend colors
+
+boxplot(log10(MyDF$Predator.mass), xlab = "Location", ylab = "log10(Predator Mass)", main = "Predator mass")
+
+
+boxplot(log(MyDF$Predator.mass) ~ MyDF$Location, # Why the tilde?
+        xlab = "Location", ylab = "Predator Mass",
+        main = "Predator mass by location")
+
+
+par(fig=c(0,0.8,0,0.8)) # specify figure size as proportion
+plot(log(MyDF$Predator.mass),log(MyDF$Prey.mass), xlab = "Predator Mass (g)", ylab = "Prey Mass (g)") # Add labels
+par(fig=c(0,0.8,0.4,1), new=TRUE)
+boxplot(log(MyDF$Predator.mass), horizontal=TRUE, axes=FALSE)
+par(fig=c(0.55,1,0,0.8),new=TRUE)
+boxplot(log(MyDF$Prey.mass), axes=FALSE)
+mtext("Fancy Predator-prey scatterplot", side=3, outer=TRUE, line=-3)
+
+pdf("../Results/Pred_Prey_Overlay.pdf", # Open blank pdf page using a relative path
+    11.7, 8.3) # These numbers are page dimensions in inches
+hist(log(MyDF$Predator.mass), # Plot predator histogram (note 'rgb')
+     xlab="Body Mass (g)", ylab="Count", col = rgb(1, 0, 0, 0.5), main = "Predator-Prey Size Overlap") 
+hist(log(MyDF$Prey.mass), # Plot prey weights
+     col = rgb(0, 0, 1, 0.5), 
+     add = T)  # Add to same plot = TRUE
+legend('topleft',c('Predators','Prey'), # Add legend
+       fill=c(rgb(1, 0, 0, 0.5), rgb(0, 0, 1, 0.5))) 
+graphics.off(); #you can also use dev.off() 
+
